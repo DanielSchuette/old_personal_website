36b5cdb757097d5a7352bbe05eee7c8c9e4b7455
Daniel Schuette <d.schuette@online.de>
2018-09-06 21:09:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DanielSchuette.github.io
+[Visit my website!](https://danielschuette.github.io)
---
+++ scripts/dropdown.js
@@ -0,0 +1,22 @@
+/* When the user clicks on dropdown button, toggle between hiding and showing the dropdown content */
+function openDropdown() {
+    document.getElementById("Header_dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown_items");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/* Debugging */
+console.log("Successfully called script from index.html")
